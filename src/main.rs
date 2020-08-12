d7897585ddd318121f0b4c8c9118c7fa774a3c7b
zaszi <admin@zaszi.net>
2020-08-12 16:37:23 +0200

    Normalize formatting

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,28 @@
+use std::collections::{HashMap, HashSet, VecDeque};
 use std::fs::File;
 use std::io::prelude::*;
-use std::collections::{VecDeque, HashMap, HashSet};
 
-use structopt::StructOpt;
-use alpm::{SigLevel, Package};
-use anyhow::{Context, Result, Error};
+use alpm::{Package, SigLevel};
+use anyhow::{Context, Error, Result};
+use petgraph::dot::{Config, Dot};
 use petgraph::graph::DiGraph;
-use petgraph::dot::{Dot, Config};
 use petgraph::visit::Bfs;
-
+use structopt::StructOpt;
 
 const ROOT_DIR: &str = "/";
 const DB_PATH: &str = "/var/lib/pacman/";
 
-fn find_package_anywhere<'a>(pkgname: &str, pacman: &'a alpm::Alpm) -> Result<Package<'a>, alpm::Error> {
-
+fn find_package_anywhere<'a>(
+    pkgname: &str,
+    pacman: &'a alpm::Alpm,
+) -> Result<Package<'a>, alpm::Error> {
     let dbs = pacman.syncdbs();
     for db in dbs {
         let maybe_pkg = db.pkg(pkgname);
         if maybe_pkg.is_ok() {
             return maybe_pkg;
         }
-    };
+    }
     Err(alpm::Error::PkgNotFound)
 }
 
@@ -30,15 +31,20 @@ fn get_reverse_deps_map(pacman: &alpm::Alpm) -> Result<HashMap<String, Vec<Strin
     let dbs = pacman.syncdbs();
 
     for db in dbs {
-        for pkg in db.pkgs().context(format!("Unable to get packages from sync db {}", db.name()))? {
+        for pkg in db
+            .pkgs()
+            .context(format!("Unable to get packages from sync db {}", db.name()))?
+        {
             for dep in pkg.depends() {
-                reverse_deps.entry(dep.name().to_string())
+                reverse_deps
+                    .entry(dep.name().to_string())
                     .and_modify(|e| e.push(pkg.base().unwrap_or_else(|| pkg.name()).to_string()))
                     .or_insert_with(|| vec![pkg.base().unwrap_or_else(|| pkg.name()).to_string()]);
             }
 
             for dep in pkg.makedepends() {
-                reverse_deps.entry(dep.name().to_string())
+                reverse_deps
+                    .entry(dep.name().to_string())
                     .and_modify(|e| e.push(pkg.base().unwrap_or_else(|| pkg.name()).to_string()))
                     .or_insert_with(|| vec![pkg.base().unwrap_or_else(|| pkg.name()).to_string()]);
             }
@@ -69,10 +75,9 @@ fn main() -> Result<()> {
     let pkgnames = args.pkgnames;
 
     let pacman = match args.dbpath {
-        Some(path) => {
-            alpm::Alpm::new(ROOT_DIR, &path).context("could not initialise pacman db from dbpath")?
-        }
-        None => alpm::Alpm::new(ROOT_DIR, DB_PATH).context("could not initialise pacman db")?
+        Some(path) => alpm::Alpm::new(ROOT_DIR, &path)
+            .context("could not initialise pacman db from dbpath")?,
+        None => alpm::Alpm::new(ROOT_DIR, DB_PATH).context("could not initialise pacman db")?,
     };
 
     let _core = pacman.register_syncdb("core", SigLevel::DATABASE_OPTIONAL);
@@ -91,7 +96,7 @@ fn main() -> Result<()> {
     let mut to_build = HashSet::new();
     to_visit.extend(pkgnames.iter().map(|x| x.as_str()));
 
-    let mut cache_node = HashMap::new(); 
+    let mut cache_node = HashMap::new();
 
     while !to_visit.is_empty() {
         let pkg = if let Some(pkg) = to_visit.pop_front() {
@@ -100,7 +105,10 @@ fn main() -> Result<()> {
             break;
         };
 
-        let root = cache_node.entry(pkg).or_insert_with(|| graph.add_node(pkg)).clone();
+        let root = cache_node
+            .entry(pkg)
+            .or_insert_with(|| graph.add_node(pkg))
+            .clone();
 
         if let Some(rev_deps_for_pkg) = reverse_deps_map.get(pkg) {
             if to_build.get(&pkg.to_string()).is_none() {
@@ -108,7 +116,10 @@ fn main() -> Result<()> {
             }
 
             for rev_dep in rev_deps_for_pkg {
-                let depnode = cache_node.entry(rev_dep).or_insert_with(|| graph.add_node(rev_dep)).clone();
+                let depnode = cache_node
+                    .entry(rev_dep)
+                    .or_insert_with(|| graph.add_node(rev_dep))
+                    .clone();
                 if !graph.contains_edge(root, depnode) {
                     graph.add_edge(root, depnode, 1);
                 }
