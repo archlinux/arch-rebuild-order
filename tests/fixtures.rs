a3be4a0dcc75351682fb98a3ebc5d32fd4a2b887
Jelle van der Waa <jelle@vdwaa.nl>
2020-10-20 22:39:20 +0200

    Add unit tests for basic rebuild order
    Create a basic test suite for rebuilder using rstest which works as
    following. With the tarfile crate the tests create fake pacman sync db's
    to be used by the main rebuild function to create various test
    scenario's.
    
    Closes: #8

---
+++ tests/fixtures.rs
@@ -0,0 +1,266 @@
+use rstest::fixture;
+use std::convert::TryFrom;
+use std::fmt;
+use std::fs;
+use std::fs::File;
+use std::io::Write;
+use tar::{Builder, Header};
+use tempfile::{tempdir, TempDir};
+
+// pacman database version (lib/libalpm/be_local.c)
+const ALPM_DB_VERSION: &str = "9";
+
+#[derive(Hash, Clone, Eq, PartialEq, Debug)]
+pub struct Package {
+    pub name: String,
+    pub base: String,
+    pub version: String,
+    pub depends: Vec<String>,
+    pub makedepends: Vec<String>,
+}
+
+impl Package {
+    fn new(
+        name: &str,
+        base: &str,
+        version: &str,
+        depends: Vec<String>,
+        makedepends: Vec<String>,
+    ) -> Package {
+        Package {
+            name: name.to_string(),
+            base: base.to_string(),
+            version: version.to_string(),
+            depends,
+            makedepends,
+        }
+    }
+
+    fn desc(&self) -> String {
+        let mut desc = String::from("");
+
+        let name = format!("%NAME%\n{}\n", self.name);
+        desc.push_str(&name);
+
+        let base = format!("%BASE%\n{}\n", self.base);
+        desc.push_str(&base);
+
+        if !self.depends.is_empty() {
+            desc.push_str("%DEPENDS%\n");
+            for dep in self.depends.iter() {
+                desc.push_str(dep);
+                desc.push_str("\n");
+            }
+            desc.push_str("\n");
+        }
+
+        if !self.makedepends.is_empty() {
+            desc.push_str("%MAKEDEPENDS%\n");
+            for dep in self.makedepends.iter() {
+                desc.push_str(dep);
+                desc.push_str("\n");
+            }
+            desc.push_str("\n");
+        }
+
+        desc
+    }
+
+    fn path(&self) -> String {
+        format!("{}-{}/desc", self.name, self.version)
+    }
+
+    fn tarheader(&self) -> Header {
+        let mut header = Header::new_gnu();
+        let desc = self.desc();
+        let datalen = u64::try_from(desc.len()).unwrap();
+        header.set_path(self.path()).unwrap();
+        header.set_size(datalen);
+        header.set_mode(0o644);
+        header.set_cksum();
+
+        header
+    }
+}
+
+impl fmt::Display for Package {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.name)
+    }
+}
+
+impl PartialEq<&str> for Package {
+    fn eq(&self, other: &&str) -> bool {
+        &self.name.as_str() == other
+    }
+}
+
+fn init_repodb(reponame: String, packages: Vec<Package>) -> (TempDir, String) {
+    let tempdir = tempdir().unwrap();
+    let dbpath = tempdir.path().display().to_string();
+
+    // local dir
+    let localdir = tempdir.path().join("local");
+    fs::create_dir(&localdir).unwrap();
+
+    let mut file = File::create(localdir.join("ALPM_DB_VERSION")).unwrap();
+    file.write_all(ALPM_DB_VERSION.as_bytes()).unwrap();
+
+    // sync dir
+    let syncdir = tempdir.path().join("sync");
+    fs::create_dir(&syncdir).unwrap();
+
+    let dbloc = syncdir.join(format!("{}.db", reponame));
+    create_db(dbloc.display().to_string(), packages);
+
+    (tempdir, dbpath)
+}
+
+fn create_db(dbloc: String, pkgs: Vec<Package>) {
+    let mut archive = Builder::new(Vec::new());
+
+    for pkg in pkgs {
+        let header = pkg.tarheader();
+        let desc = pkg.desc();
+        let data = desc.as_bytes();
+        archive.append(&header, data).unwrap();
+    }
+
+    archive.finish().unwrap();
+    let data = archive.into_inner().unwrap();
+
+    let mut afile = File::create(dbloc).unwrap();
+    afile.write_all(&data).unwrap();
+}
+
+#[fixture]
+pub fn invalid_dbpath() -> (Vec<String>, Option<String>) {
+    let pkgnames = vec![String::from("testpkg1")];
+    let dbpath = Some(String::from("/non-existant-path"));
+
+    (pkgnames, dbpath)
+}
+
+#[fixture]
+pub fn no_reverse_deps() -> (Vec<Package>, Option<String>, Vec<String>, TempDir) {
+    let testpkg = Package::new("testpkg1", "testpkg1", "1.0-1", vec![], vec![]);
+    let packages = vec![testpkg];
+
+    let reponame = "test";
+    let (rootdir, dbpath) = init_repodb(reponame.to_string(), packages.clone());
+
+    (packages, Some(dbpath), vec![reponame.to_string()], rootdir)
+}
+
+#[fixture]
+pub fn reverse_deps() -> (Vec<String>, Option<String>, Vec<String>, TempDir) {
+    let testpkg = Package::new("testpkg1", "testpkg1", "1-1", vec![], vec![]);
+    let testpkg2 = Package::new(
+        "testpkg2",
+        "testpkg2",
+        "1-1",
+        vec![testpkg.name.clone()],
+        vec![],
+    );
+    let pkgnames = vec![testpkg.name.clone(), testpkg2.name.clone()];
+    let packages = vec![testpkg, testpkg2];
+
+    let reponame = "test";
+    let (tempdir, dbpath) = init_repodb(reponame.to_string(), packages);
+
+    (pkgnames, Some(dbpath), vec![reponame.to_string()], tempdir)
+}
+
+#[fixture]
+pub fn multiple_deps() -> (Vec<Package>, Option<String>, Vec<String>, TempDir) {
+    let testpkg = Package::new("testpkg1", "testpkg1", "1.0-1", vec![], vec![]);
+    let testpkg2 = Package::new(
+        "testpkg2",
+        "testpkg2",
+        "1.0-1",
+        vec![testpkg.name.clone()],
+        vec![],
+    );
+    let testpkg3 = Package::new(
+        "testpkg3",
+        "testpkg3",
+        "1-1",
+        vec![testpkg.name.clone(), testpkg2.name.clone()],
+        vec![],
+    );
+    let packages = vec![testpkg3, testpkg2, testpkg];
+
+    let reponame = "test";
+    let (tempdir, dbpath) = init_repodb(reponame.to_string(), packages.clone());
+
+    (packages, Some(dbpath), vec![reponame.to_string()], tempdir)
+}
+
+#[fixture]
+pub fn reverse_make_deps() -> (Vec<Package>, Option<String>, Vec<String>, TempDir) {
+    let testpkg = Package::new("testpkg1", "testpkg1", "1-1", vec![], vec![]);
+    let testpkg2 = Package::new(
+        "testpkg2",
+        "testpkg2",
+        "1-1",
+        vec![],
+        vec![testpkg.name.clone()],
+    );
+    let packages = vec![testpkg, testpkg2];
+
+    let reponame = "test";
+    let (tempdir, dbpath) = init_repodb(reponame.to_string(), packages.clone());
+
+    (packages, Some(dbpath), vec![reponame.to_string()], tempdir)
+}
+
+#[fixture]
+pub fn dependency_depth() -> (Vec<Package>, Option<String>, Vec<String>, TempDir) {
+    let testpkg = Package::new("testpkg1", "testpkg1", "1-1", vec![], vec![]);
+    let testpkg2 = Package::new(
+        "testpkg2",
+        "testpkg2",
+        "1-1",
+        vec![],
+        vec![testpkg.name.clone()],
+    );
+    let testpkg3 = Package::new(
+        "testpkg3",
+        "testpkg3",
+        "1-1",
+        vec![],
+        vec![testpkg2.name.clone()],
+    );
+    let packages = vec![testpkg, testpkg2, testpkg3];
+
+    let reponame = "test";
+    let (tempdir, dbpath) = init_repodb(reponame.to_string(), packages.clone());
+
+    (packages, Some(dbpath), vec![reponame.to_string()], tempdir)
+}
+
+#[fixture]
+pub fn dependency_cycle() -> (Vec<Package>, Option<String>, Vec<String>, TempDir) {
+    let testpkg = Package::new(
+        "testpkg1",
+        "testpkg1",
+        "1-1",
+        vec![String::from("testpkg2")],
+        vec![],
+    );
+    let testpkg2 = Package::new(
+        "testpkg2",
+        "testpkg2",
+        "1-1",
+        vec![testpkg.name.clone()],
+        vec![],
+    );
+    dbg!(testpkg.clone());
+    dbg!(testpkg2.clone());
+    let packages = vec![testpkg, testpkg2];
+
+    let reponame = "test";
+    let (tempdir, dbpath) = init_repodb(reponame.to_string(), packages.clone());
+
+    (packages, Some(dbpath), vec![reponame.to_string()], tempdir)
+}
---
+++ tests/test_rebuilder.rs
@@ -0,0 +1,124 @@
+use rstest::rstest;
+use tempfile::TempDir;
+
+pub mod fixtures;
+
+use fixtures::{
+    dependency_cycle, dependency_depth, invalid_dbpath, multiple_deps, no_reverse_deps,
+    reverse_deps, reverse_make_deps, Package,
+};
+
+#[rstest]
+#[should_panic]
+fn test_invalid_dbpath(invalid_dbpath: (Vec<String>, Option<String>)) {
+    let pkgnames = invalid_dbpath.0;
+    let dbpath = invalid_dbpath.1;
+    rebuilder::run(pkgnames, dbpath, vec![], None).unwrap();
+}
+
+/// A package without any reverse dependencies should only print the given package
+#[rstest]
+fn test_no_reverse_deps(no_reverse_deps: (Vec<Package>, Option<String>, Vec<String>, TempDir)) {
+    let packages = no_reverse_deps.0;
+
+    let res = rebuilder::run(
+        vec![packages[0].name.clone()],
+        no_reverse_deps.1,
+        no_reverse_deps.2,
+        None,
+    )
+    .unwrap();
+    assert_eq!(packages[0], res.trim());
+}
+
+/// Given a package 'testpkg1' with a reverse dependency on 'testpkg2', the rebuild order should be
+/// 'testpkg1 testpkg2'
+#[rstest]
+fn test_reverse_deps(reverse_deps: (Vec<String>, Option<String>, Vec<String>, TempDir)) {
+    let pkgnames = reverse_deps.0;
+    let pkgname = &pkgnames[0];
+
+    let res = rebuilder::run(
+        vec![pkgname.to_string()],
+        reverse_deps.1,
+        reverse_deps.2,
+        None,
+    )
+    .unwrap();
+    let res_pkgs: Vec<&str> = res.trim().split_ascii_whitespace().collect();
+    assert_eq!(pkgnames, res_pkgs);
+}
+
+/// Given a package 'testpkg1' with a reverse make dependency on 'testpkg2', the rebuild order
+/// should be 'testpkg1 testpkg2'
+#[rstest]
+fn test_reverse_make_deps(reverse_make_deps: (Vec<Package>, Option<String>, Vec<String>, TempDir)) {
+    let packages = reverse_make_deps.0;
+    let pkgname = &packages[0].name;
+
+    let res = rebuilder::run(
+        vec![pkgname.to_string()],
+        reverse_make_deps.1,
+        reverse_make_deps.2,
+        None,
+    )
+    .unwrap();
+    let res_pkgs: Vec<&str> = res.trim().split_ascii_whitespace().collect();
+    assert_eq!(packages, res_pkgs);
+}
+
+/// Given a package 'testpkg1' with a reverse dependency of 'testpkg2' and 'testpkg3', the rebuild
+/// order should be 'testpkg1 'testpkg2 testpkg3'
+#[rstest]
+fn test_multiple_deps(multiple_deps: (Vec<Package>, Option<String>, Vec<String>, TempDir)) {
+    let packages = multiple_deps.0;
+    let pkgname = &packages[0];
+
+    let res = rebuilder::run(
+        vec![pkgname.to_string()],
+        multiple_deps.1,
+        multiple_deps.2,
+        None,
+    )
+    .unwrap();
+    let res_pkgs: Vec<&str> = res.trim().split_ascii_whitespace().collect();
+    assert_eq!(packages[0], res_pkgs[0]);
+}
+
+/// Given a package 'testpkg1' with a reverse dependency of 'testpkg2' which has a reverse
+/// dependency on 'testpkg3' the rebuild order should be 'testpkg1 'testpkg2 testpkg3'
+#[rstest]
+fn test_dependency_depth(dependency_depth: (Vec<Package>, Option<String>, Vec<String>, TempDir)) {
+    let packages = dependency_depth.0;
+    let pkgname = &packages[0];
+
+    let res = rebuilder::run(
+        vec![pkgname.to_string()],
+        dependency_depth.1,
+        dependency_depth.2,
+        None,
+    )
+    .unwrap();
+    let res_pkgs: Vec<&str> = res.trim().split_ascii_whitespace().collect();
+    assert_eq!(packages[0], res_pkgs[0]);
+}
+
+/// Given a package 'testpkg1' with a dependency on 'testpkg2' and 'testpkg2' having a dependency
+/// on 'testpkg1'. Providing 'testpkg1' should return 'testpkg1 testpkg2'
+/// TODO: There should be a warning that there is a dependency cycle and the dep cycle should be
+/// broken #3.
+#[rstest]
+fn test_dependency_cycle(dependency_cycle: (Vec<Package>, Option<String>, Vec<String>, TempDir)) {
+    let packages = dependency_cycle.0;
+    let pkgname = &packages[0];
+
+    let res = rebuilder::run(
+        vec![pkgname.to_string()],
+        dependency_cycle.1,
+        dependency_cycle.2,
+        None,
+    )
+    .unwrap();
+    let res_pkgs: Vec<&str> = res.trim().split_ascii_whitespace().collect();
+    assert_eq!(packages[0], res_pkgs[0]);
+}
