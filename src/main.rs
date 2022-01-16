80d95b481a43ccea2713bd2a5218e6b6bdb6bf09
Jelle van der Waa <jelle@archlinux.org>
2022-01-16 16:46:20 +0100

    Use Rust 1.58 format strings

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ fn main() {
     let args = Args::from_args();
     match arch_rebuild_order::run(args.pkgnames, args.dbpath, args.repos, args.dotfile) {
         Ok(output) => {
-            println!("{}", output);
+            println!("{output}");
             std::process::exit(0);
         }
         Err(e) => {
